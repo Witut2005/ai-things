ee455445e27687e0866164e2b13c7c954f0086e8
Witut2005 <wiktor.glogowski@onet.pl>
2023-06-18 18:28:43 +0200

    ant colony

--- ants/ant.cpp
+++ ants/ant.cpp
@@ -1,6 +1,9 @@
 
 #include <bits/stdc++.h>
 
+#define PHEROMONES_FACTOR 1
+#define HEURISTICS_FACTOR 2
+
 enum ATTRACTIONS
 {
     CIRCUS = 0,
@@ -18,7 +21,10 @@ class Ant
     std::vector<uint32_t> Places;
     uint32_t distance;
     uint32_t** distance_matrix;
+    std::pair<double**, std::pair<uint32_t, uint32_t>> pheromones;
     uint32_t current_place;
+    uint32_t next_place;
+    std::vector<uint32_t> PlacesVisited;
 
     int get_random_int(int min, int max)
     {
@@ -28,24 +34,76 @@ class Ant
         return uniform_dist(e1);
     }
 
-    int probability_roulette[10][10];
+    int* probability_roulette;
 
     public:
 
-    Ant(std::vector<uint32_t> Places, uint32_t** distance_matrix, uint32_t first_place_id) : Places(Places), distance(0), distance_matrix(distance_matrix), current_place(first_place_id)
+    Ant(std::vector<uint32_t> Places, uint32_t** distance_matrix, std::pair<double**, std::pair<uint32_t, uint32_t>> pheromones, uint32_t first_place_id) : Places(Places), distance(0), distance_matrix(distance_matrix), pheromones(pheromones), current_place(first_place_id)
     {
         this->Places.erase(std::find(this->Places.begin(), this->Places.end(), current_place));
-        std::cout << current_place << " -> ";
+        this->PlacesVisited.push_back(this->current_place);
+        this->probability_roulette = new int[100];
+        // std::cout << this->current_place << " -> ";
     }
 
-    void calculate_probabilty(void)
+    ~Ant()
+    {
+        delete []this->probability_roulette;
+    }
+
+    void calculate_probability(void)
     {
         memset(this->probability_roulette, -1, 100 * sizeof(int));
+
+        std::vector<std::pair<uint32_t, double>> Values;
+        double all_probabilities = 0;
+
+        for(const auto& a : this->Places)
+        {
+            double probability = pow(pheromones.first[this->current_place][a], PHEROMONES_FACTOR) * pow(1 / (double)distance_matrix[this->current_place][a], HEURISTICS_FACTOR) * 10;
+            all_probabilities += probability;
+            Values.push_back(std::make_pair(a, probability));
+        }
+
+        uint32_t index = 0;
+        for(auto it = Values.begin(); it != Values.end(); it++)
+        {
+
+            uint32_t start_index = index;
+            uint32_t end_index = start_index + round(it->second / all_probabilities * 100);
+
+            if(it == std::prev(Values.end(), 1))
+                end_index = 100;
+
+            for(; index < end_index; index++)
+                this->probability_roulette[index] = it->first;
+        }
+
     }
 
-    void visit_using_pheromones(uint32_t** pheromones)
+    void visit_using_pheromones()
     {
 
+    }
+
+    void print_probabilities(void)
+    {
+        for(int i = 0; i < 10; i++) 
+        {
+            for(int j = 0; j < 10; j++) 
+                std::cout << this->probability_roulette[i * 10 + j] << " ";
+            std::cout << std::endl;
+        }
+    }
+
+    void print_pheromones(void)
+    {
+        for(uint32_t i = 0; i < pheromones.second.first; i++) 
+        {
+            for(uint32_t j = 0; j < pheromones.second.second; j++) 
+                std::cout << this->pheromones.first[i][j] << " ";
+            std::cout << std::endl;
+        }
     }
 
     void visit_random(void)
@@ -56,7 +114,7 @@ class Ant
         
         auto PlaceSelected = this->Places.begin() + (get_random_int(0, this->Places.size() - 1));
 
-        std::cout << *PlaceSelected << " -> ";
+        // std::cout << *PlaceSelected << " -> ";
         
         this->distance = this->distance + this->distance_matrix[this->current_place][*PlaceSelected];
         this->current_place = *PlaceSelected;
@@ -66,8 +124,45 @@ class Ant
         //     std::cout << a << " ";
         // std::cout << std::endl;
         // std::cout << this->Places.size() << std::endl;
+    }
 
+    void visit(void)
+    {
 
+        if(!this->Places.size())
+            return;
+
+        this->calculate_probability();
+        // this->print_probabilities();
+        // std::cout << "----------------------------------------------------------------" << std::endl;
+
+        auto PlaceSelected = std::find(this->Places.begin(), this->Places.end(), this->probability_roulette[this->get_random_int(0, 100)]);
+        
+        if(PlaceSelected == this->Places.end())
+            return;
+
+        // if(this->Places.size() > 1)
+        //     std::cout << *PlaceSelected << " -> ";
+        // else
+        //     std::cout << *PlaceSelected << std::endl;
+
+        this->PlacesVisited.push_back(*PlaceSelected);
+        
+        this->distance = this->distance + this->distance_matrix[this->current_place][*PlaceSelected];
+        this->current_place = *PlaceSelected;
+        this->Places.erase(PlaceSelected);
+
+        if(!this->Places.size())
+        {
+            for(auto it = this->PlacesVisited.begin(); it != this->PlacesVisited.end() - 1; it++)
+                this->pheromones.first[*it][*(it+1)] += (double)100 / (double)this->distance;
+        }
+
+    }
+
+    auto get_visited(void)
+    {
+        return this->PlacesVisited;
     }
 
     bool if_all_places_visited(void)
--- ants/main.cpp
+++ ants/main.cpp
@@ -4,13 +4,13 @@
 
 #define ITEMS_COUNT 6
 
-template<int COLS, int ROWS>
-uint32_t** convert_arr_to_ptr(uint32_t array[COLS][ROWS]) 
+template<typename T, int COLS, int ROWS>
+T** convert_arr_to_ptr(T array[COLS][ROWS]) 
 {
-    uint32_t** ptr = new uint32_t*[ROWS];
+    T** ptr = new T*[ROWS];
     for (int i = 0; i < ROWS; ++i) 
     {
-        ptr[i] = new uint32_t[COLS];
+        ptr[i] = new T[COLS];
         for (int j = 0; j < COLS; ++j) 
             ptr[i][j] = array[i][j];
     }
@@ -29,6 +29,14 @@ int main(void)
         {4,5,7,6,3,0}
     };
 
+    double pheromones[ITEMS_COUNT][ITEMS_COUNT];
+
+    for(int i = 0; i < ITEMS_COUNT; i++)
+    {
+        for(int j = 0; j < ITEMS_COUNT; j++)
+            pheromones[i][j] = 1;
+    }
+
     // std::vector<std::pair<uint32_t, std::string>> Attractions = 
     // {
     //     {CIRCUS, "circus"},
@@ -40,14 +48,43 @@ int main(void)
  
     std::vector<uint32_t> Attractions = { CIRCUS, BALLON, CARS, CAROUSEL, CHAIN_CAROUSEL, FERRIS_WHEEL };
 
-    Ant* ant = new Ant(Attractions, convert_arr_to_ptr<6,6>(distance_between_attractions), 0);
+    double** ants_pheromones = convert_arr_to_ptr<double, ITEMS_COUNT, ITEMS_COUNT>(pheromones);
+    uint32_t** ants_distance = convert_arr_to_ptr<uint32_t, ITEMS_COUNT,ITEMS_COUNT>(distance_between_attractions);
+
+    Ant* ant;
+
+    for(int i = 0; i < 1000; i++)
+    {
+        ant = new Ant(Attractions, ants_distance, std::pair<double**, std::pair<uint32_t, uint32_t>> (ants_pheromones, {ITEMS_COUNT, ITEMS_COUNT}), CHAIN_CAROUSEL);
+
+        while(!ant->if_all_places_visited())
+            ant->visit();
+
+        delete ant;
+    }
+
+    ant = new Ant(Attractions, ants_distance, std::pair<double**, std::pair<uint32_t, uint32_t>> (ants_pheromones, {ITEMS_COUNT, ITEMS_COUNT}), CHAIN_CAROUSEL);
 
     while(!ant->if_all_places_visited())
     {
-        ant->visit_random();
+        ant->visit();
     }
+
+    for(const auto& a : ant->get_visited())
+        std::cout << a << " -> ";
+
     std::cout << std::endl;
-    std::cout << ant->get_distance() << std::endl;
+
+
+    for(int i = 0; i < ITEMS_COUNT; i++)
+    {
+        delete ants_pheromones[i];
+        delete ants_distance[i];
+    }
+
+    delete ants_pheromones;
+    delete ants_distance;
+
 
 }
 
