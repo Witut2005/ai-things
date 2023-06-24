700d2bb5e55529031b786528cc1be3a26aafd9aa
Witut2005 <wiktor.glogowski@onet.pl>
2023-06-24 15:25:09 +0200

    pso alg

--- particles/main.cpp
+++ particles/main.cpp
@@ -1,21 +1,40 @@
 
+#include <unistd.h>
 #include <bits/stdc++.h>
 #include "./particle/particle.cpp"
 
 int main(void)
 {
-    int number_of_particles = 50;
+    uint64_t number_of_particles = 1000000;
 
     Particle* particles = new Particle[number_of_particles];
 
-    std::pair<double, double> PositionToFind = {500.0, 500.0};
+    std::pair<double, double> PositionToFind = {1000, 500000.0 / 2};
 
     std::default_random_engine generator;
-    std::uniform_real_distribution<double> distribution(-10.0, 10.0);
+    std::uniform_real_distribution<double> distribution(0.0, 500000.0);
+
+    // for(int i = 0; i < number_of_particles; i++)
+    //     std::cout << distribution(generator) << std::endl;
 
     for(int i = 0; i < number_of_particles; i++)
-        particles[i] = *new Particle(std::make_pair(distribution(generator), distribution(generator)), 1, 2, 1);
+        particles[i] = *new Particle(PositionToFind, std::make_pair(distribution(generator), distribution(generator)), 1, 2, 1);
 
+    std::cout << Particle::get_sworm_best().first << " " << Particle::get_sworm_best().second << std::endl;
 
+    while(Particle::get_sworm_best() != PositionToFind)
+    {
 
+        auto best = Particle::get_sworm_best();
+
+        for(int i = 0; i < number_of_particles; i++)
+            particles[i].update_position();
+
+        for(int i = 0; i < number_of_particles; i++)
+        {
+            if(Particle::calculate_distance(PositionToFind, particles[i].get_position()) < Particle::calculate_distance(PositionToFind, best))
+                std::cout << particles[i].get_position().first << " " << particles[i].get_position().second << std::endl;
+        }
+    }
+    std::cout << Particle::get_sworm_best().first << " " << Particle::get_sworm_best().second << std::endl; 
 }
--- particles/particle/particle.cpp
+++ particles/particle/particle.cpp
@@ -5,31 +5,68 @@
 class Particle
 {
     private:
-    static double sworm_best; 
+    static std::pair<double, double> SwormBest; 
 
     double inertia;
     double cognitive_const;
     double social_const;
+
     double velocity;
+    std::pair<double, double> Position;
+    std::pair<double, double> Destination;
+    std::pair<double, double> PersonalBest;
 
     public:
 
     Particle(){}
 
-    Particle(std::pair<double, double> vector, const double inertia, const double cognitive_const, const double social_const) : inertia(inertia), cognitive_const(cognitive_const), social_const(social_const)
+    Particle(std::pair<double, double> Destination, std::pair<double, double> Position, const double inertia, const double cognitive_const, const double social_const) : Destination(Destination), Position(Position), inertia(inertia), 
+                cognitive_const(cognitive_const), social_const(social_const)
     {
-        
+        velocity = 0;
+
+        if(calculate_distance(this->Destination, this->Position) < calculate_distance(this->Destination, this->SwormBest))
+            this->SwormBest = this->Position;
+
+    }
+
+    static double calculate_distance(std::pair<double, double> Destination, std::pair<double, double> CurrentPosition)
+    {
+        return sqrt(pow(CurrentPosition.first - Destination.first, 2) + pow(CurrentPosition.second - Destination.second, 2));
+    }
+
+    static std::pair<double, double> get_sworm_best(void)
+    {
+        return Particle::SwormBest;
     }
 
     void update_position(void)
     {
+        this->update_velocity();
 
+        this->Position.first += this->velocity;
+        this->Position.second += this->velocity;
+
+        if(calculate_distance(this->Destination, this->Position) < calculate_distance(this->Destination, this->SwormBest))
+            this->SwormBest = this->Position;
+
+        
+        
+    }
+
+    auto get_position(void)
+    {
+        return this->Position;
     }
 
     void update_velocity(void)
     {
-        // double cognitive_velocity 
+        double inertia_velocity = this->inertia * this->velocity;
+        double cognitive_velocity = 0.2 * this->cognitive_const * ((this->PersonalBest.first - this->Position.first) + (this->PersonalBest.second - this->Position.second));
+        double social_velocity = 0.1 * this->social_const * sqrt((pow(this->SwormBest.first - this->Position.first, 2) + pow(this->SwormBest.second - this->Position.second, 2)));
+
+        this->velocity = inertia_velocity + cognitive_velocity + social_velocity;
     }
 };
 
-double Particle::sworm_best;
+std::pair<double, double> Particle::SwormBest = {std::numeric_limits<double>::max(), std::numeric_limits<double>::max()};
